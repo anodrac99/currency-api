2c77319c6d9edf6536998c97b72214e625dcbcf8
anodrac99 <juancho-cm2000@hotmail.com>
2022-06-30 22:10:04 -0500

    attributes off

--- controllers/historical.controllers.js
+++ controllers/historical.controllers.js
@@ -1,39 +1,34 @@
-const { Historical } = require('../models/historical.model')
+const { Historical } = require("../models/historical.model");
 
 const getHistorical = async (req, res, next) => {
-    try {
-        const info = await Historical.findAll({
-            attributes: ['name', 'value']
-        })
+  try {
+    const info = await Historical.findAll();
 
-        res.status(200).json({
-            status: 'success',
-            info
-        })
-
-    } catch (error) {
-        console.log(error);
-    }
-}
+    res.status(200).json({
+      status: "success",
+      info,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const createHistorical = async (req, res, next) => {
-    try {
-        const { name, value } = req.body;
+  try {
+    const { name, value } = req.body;
 
-        const newHistorical = await Historical.create({
-            name,
-            value
-        })
+    const newHistorical = await Historical.create({
+      name,
+      value,
+    });
 
-        res.status(201).json({
-            status: 'success',
-            newHistorical
-        })
+    res.status(201).json({
+      status: "success",
+      newHistorical,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-    } catch (error) {
-        console.log(error);
-    }
-
-}
-
-module.exports = { getHistorical, createHistorical }
+module.exports = { getHistorical, createHistorical };
--- controllers/lastest.controllers.js
+++ controllers/lastest.controllers.js
@@ -1,38 +1,34 @@
-const { Lastest } = require('../models/lastest.model')
+const { Lastest } = require("../models/lastest.model");
 
 const getLastest = async (req, res, next) => {
-    try {
-        const info = await Lastest.findAll({
-            attributes: ['name', 'value']
-        })
+  try {
+    const info = await Lastest.findAll();
 
-        res.status(200).json({
-            status: 'success',
-            info
-        })
-
-    } catch (error) {
-        console.log(error)
-    }
-}
+    res.status(200).json({
+      status: "success",
+      info,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const createLastest = async (req, res, next) => {
-    try {
-        const { name, value } = req.body
+  try {
+    const { name, value } = req.body;
 
-        const newLastest = await Lastest.create({
-            name,
-            value
-        })
+    const newLastest = await Lastest.create({
+      name,
+      value,
+    });
 
-        res.status(201).json({
-            status: 'success',
-            newLastest
-        })
-        
-    } catch (error) {
-        console.log(error)
-    }
-}
+    res.status(201).json({
+      status: "success",
+      newLastest,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-module.exports = { getLastest, createLastest }
+module.exports = { getLastest, createLastest };
